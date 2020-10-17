613c63247f6a308fc715e2dfa44dff9c09856b4d
Axel Lapel <axel.lapel@gmail.com>
2020-10-17 15:22:29 +0200

    minor changes

--- run_training.sh
+++ run_training.sh
@@ -3,11 +3,9 @@
 #SBATCH -p volta
 #SBATCH --gres=gpu:1
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=5
 #SBATCH -t 16:00:00
 #SBATCH -e ../log_train/%N.%j.%a.err
 #SBATCH -o ../log_train/%N.%j.%a.out
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=axel.lapel@oca.eu
 
 python3 training.py

d387d9b09b82369ed201e7b430d052421831faf8
Axel Lapel <axel.lapel@gmail.com>
2020-10-17 15:44:45 +0200

    changed GPU partition

--- run_training.sh
+++ run_training.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #SBATCH -J Job_training_rNVP
-#SBATCH -p volta
+#SBATCH -p kepler
 #SBATCH --gres=gpu:1
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=4
 #SBATCH -t 16:00:00
+#SBATCH -A a226
 #SBATCH -e ../log_train/%N.%j.%a.err
 #SBATCH -o ../log_train/%N.%j.%a.out
 
